d225a84fcf08fbe6b96f5f02f6f4c388c70e2b50
Johhan Santana <johhan.santana@gmail.com>
2019-05-14 15:46:22 -0400

    commit

---
+++ src/TypeTest.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+
+type TitleProps = {
+  title: string,
+  openNow: boolean
+};
+
+type CardProps = {
+  title: TitleProps
+};
+
+export default (props: CardProps) => <p>hello</p>;
---
+++ src/TypeTest2.js
@@ -0,0 +1,7 @@
+// @flow
+import React from "react";
+import TypeTest from './TypeTest'
+
+// Try ctrl + space to see suggestions here it won't give you the correct ones
+// and it won't typecheck either
+export default () => <TypeTest title={{}} />;
